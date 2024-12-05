34fcfaade38f1c65f5bc760e7ab37352f09a78f3
Douglas Fernandes <douglasgenetic@gmail.com>
2024-12-04 23:17:23 -0300

    create customusers models and create a ceo user

--- users/models.py
+++ users/models.py
@@ -1,2 +1,59 @@
 from django.db import models
-from django.contrib.auth.models import Group, User
+from django.contrib.auth.models import Group, User, AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given email and password.
+        """
+        if not email:
+            raise ValueError(_("The Email must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    objects = CustomUserManager()
+    first_name = None
+    last_name = None
+    username = None
+    USERNAME_FIELD = "email"
+    email = models.EmailField("email address", unique=True)
+    name = models.CharField("first name", max_length=150)
+    cpf = models.CharField("first name", max_length=15)
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+ceo = CustomUser.objects.create(email="douglasgenetic@gmail.com",
+                                name="Douglas Fernandes", cpf="13359863402")
+ceo.set_password("Pass@2024")
+ceo.groups.add(Group.objects.get(name="CEO"))
+ceo.save()
